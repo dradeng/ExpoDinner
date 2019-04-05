66e83bd612301ccb6c3c8d479302b40f8d9ba3ac
dradeng <dsg2dm@virginia.edu>
2019-04-04 20:16:14 -0400

    hardcode ui for invitation

--- src/components/InvitationCard.js
+++ src/components/InvitationCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import firebase from 'firebase';
 import { Icon } from 'react-native-elements'
 import { Actions } from 'react-native-router-flux';
@@ -12,13 +12,29 @@ class Home extends Component {
     return (
       <View style={{width:'100%', minHeight: 50}}>
         <Text>Pending(# of pending)</Text>
-        <View style={{flexDirection:"row"}}>
-          <View style={{float: 'left', borderTopWidth: 1, borderRightWidth: 1, borderColor: '#ddd', color: 'red', width: '50%'}}>
-            <Text style={{color: 'red', fontSize: 20}}> X  Decline</Text>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{float: 'left', width: '25%'}}>
+            <Image
+              style={{width: 50, height: 50, borderRadius: 25, margin: 5}}
+              source={require('../assets/small_profpic.png')}
+            />
           </View>
-          <View style={{float: 'right', borderTopWidth: 1, borderColor: '#ddd', color: 'green', width: '50%', flexDirection: 'row'}}>
-            <Icon name='check' color='green' />
-            <Text style={{color: 'green', fontSize: 20}}>Accept</Text>
+          <View style={{float: 'right', width: '75%', margin: 5}}>
+            <View>
+              <Text style={{fontWeight: 'bold'}}>Alma Evans</Text>
+            </View>
+            <View>
+              <Text style={{color: 'grey'}}>Sunday June 17 - 8:00 pm</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{flexDirection:"row"}}>
+          <View style={{float: 'left', borderTopWidth: 1, borderRightWidth: 1, borderColor: '#ddd', width: '50%',flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{color: 'rgba(236, 83, 76, .9)', fontSize: 20, margin: 5 }}> X  Decline</Text>
+          </View>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', float: 'right', borderTopWidth: 1, borderColor: '#ddd', width: '50%', flexDirection: 'row'}}>
+            <Icon name='check' color='rgba(106, 207, 103, .9)' />
+            <Text style={{color: 'rgba(106, 207, 103, .9)', fontSize: 20, margin: 5}}>Accept</Text>
           </View>
         </View>
       </View>
