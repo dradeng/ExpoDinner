52ae05502881eb0970446e23bfee563af68131fc
dradeng <dsg2dm@virginia.edu>
2019-04-06 18:00:41 -0400

    location appears when click on marker icon

--- src/components/CreateDinner.js
+++ src/components/CreateDinner.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
 import firebase from 'firebase';
-import { Icon } from 'react-native-elements'
+import { Icon } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
-import { TimePicker, DatePicker } from 'react-native-wheel-picker-android'
+import { TimePicker, DatePicker } from 'react-native-wheel-picker-android';
+import Geocoder from 'react-native-geocoding';
 import { Button, Card, CardSection, Spinner } from './common';
 import { Input } from 'react-native-elements';
+import { Constants, Location, Permissions } from 'expo';
 
 
 class CreateDinner extends Component {
-  state = { email: '', password: '', error: '', loading: false };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      latitude: null,
+      longitude: null,
+      address: null,
+      location: null,
+      error: null,
+    };
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+        console.log(position);
+      },
+    );
+  }
   change() {
-    //do nithing
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + this.state.latitude + ',' + this.state.longitude + '&key=' + 'AIzaSyBl9N_4Qo5m-IElGXKpz62Ah42YaUSSU2g')
+      .then((response) => response.json())
+      .then((responseJson) => {
+       
+        var addy = JSON.stringify(responseJson.results[0].formatted_address);
+        this.setState({
+          location: addy
+        });
+    })
   }
   render() {
     return (
@@ -24,9 +55,15 @@ class CreateDinner extends Component {
             <Text style={{color: 'grey', width: '100%', textAlign: 'center', paddingTop: 50}}>
               Choose a location
             </Text>
-            <Input
-              placeholder='Enter Address'
-            />
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <View style={{width: '80%'}}>
+                <Input
+                  leftIcon={ <Icon onPress={this.change.bind(this)} type='font-awesome' color='rgb(15,140,255)' name='map-marker' style={{margin: 30}}/>}
+                  placeholder='  Enter Address'
+                  value={this.state.location}
+                />
+              </View>
+            </View>
           </View>
         </View>
       </View>
