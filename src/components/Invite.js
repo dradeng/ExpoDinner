25134714c1cd160a0d5e6db374b787a9618a0cf6
dradeng <dsg2dm@virginia.edu>
2019-04-07 16:51:40 -0400

    invite created

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -7,14 +7,17 @@ import InvitationCard from './InvitationCard';
 
 class Invite extends Component {
   
-
-  highlight() {
-    Actions.login();
-  }
   render() {
     return (
-      <View>
-        <Text>Invite</Text>
+      <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
+        <View>
+          <Text style={{color: 'grey', width: '100%', textAlign: 'center'}}> 320 14th St NW</Text>
+          <Text style={{color: 'grey', width: '100%', textAlign: 'center'}}> Thurs 11 April - 9:00pm</Text>
+        </View>
+        <View style={{width: '100%'}}>
+          <Text style={{color: 'grey', float: 'left'}}>Who do you want to invite?</Text>
+          <Text style={{color: 'rgba(106, 207, 103, .9)', float:'right'}}>1 Selected</Text>
+        </View>
       </View>
     );
   }
