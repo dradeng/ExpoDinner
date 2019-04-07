a71fd1b0b1d4762e8e1e4ee9657c3c258cb6c2c1
dradeng <dsg2dm@virginia.edu>
2019-04-07 00:20:10 -0400

    map included

--- src/components/CreateDinner.js
+++ src/components/CreateDinner.js
@@ -7,16 +7,15 @@ import { TimePicker, DatePicker } from 'react-native-wheel-picker-android';
 import Geocoder from 'react-native-geocoding';
 import { Button, Card, CardSection, Spinner } from './common';
 import { Input } from 'react-native-elements';
-import { Constants, Location, Permissions } from 'expo';
-
+import { Constants, Location, Permissions,MapView } from 'expo';
 
 class CreateDinner extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      latitude: null,
-      longitude: null,
+      latitude: 38.03817,
+      longitude: -78.489014,
       address: null,
       location: null,
       error: null,
@@ -30,11 +29,36 @@ class CreateDinner extends Component {
           longitude: position.coords.longitude,
           error: null,
         });
-        console.log(position);
       },
     );
   }
+  handleSubmit() {
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + this.state.address + '&key=' + 'AIzaSyBl9N_4Qo5m-IElGXKpz62Ah42YaUSSU2g')
+      .then((response) => response.json())
+      .then((responseJson) => {
+       
+        var addy = JSON.stringify(responseJson.results[0].geometry.location);
+        console.log(addy)
+        console.log(this.state.latitude)
+        console.log(responseJson.results[0].geometry.location.lat)
+        console.log(this.state.longitude)
+        console.log(responseJson.results[0].geometry.location.lng)
+        this.setState({
+          latitude: responseJson.results[0].geometry.location.lat,
+          longitude: responseJson.results[0].geometry.location.lng
+        });
+    })
+  }
   change() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+    );
     fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + this.state.latitude + ',' + this.state.longitude + '&key=' + 'AIzaSyBl9N_4Qo5m-IElGXKpz62Ah42YaUSSU2g')
       .then((response) => response.json())
       .then((responseJson) => {
@@ -54,19 +78,33 @@ class CreateDinner extends Component {
             <TimePicker/>
             <Text style={{color: 'grey', width: '100%', textAlign: 'center', paddingTop: 50}}>
               Choose a location
+              
             </Text>
             <View style={{flexDirection: 'row', justifyContent: 'center'}}>
               <View style={{width: '80%'}}>
+              
                 <Input
-                  leftIcon={ <Icon onPress={this.change.bind(this)} type='font-awesome' color='rgb(15,140,255)' name='map-marker' style={{margin: 30}}/>}
+                  onChange={this.handleSubmit.bind(this)}
+                  leftIcon={<Icon onPress={this.change.bind(this)} type='font-awesome' color='rgb(15,140,255)' name='map-marker' style={{margin: 30}}/>}
                   placeholder='  Enter Address'
                   value={this.state.location}
                 />
+    
               </View>
-            </View>
+            </View>   
+              <MapView
+                style={{height: '60%', width: '100%', paddingTop: 10}}
+                initialRegion={{
+                  latitude: this.state.latitude,
+                  longitude: this.state.longitude,
+                  latitudeDelta: 0.0722,
+                  longitudeDelta: 0.0321,
+                }}
+              />
           </View>
         </View>
       </View>
+
       
     );
   }
