abe9052bb0ea9f362dd4bed1fc28b6ec8a13184c
dradeng <dsg2dm@virginia.edu>
2019-04-09 20:43:42 -0400

    select done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
+import CircleCheckBox from 'react-native-circle-checkbox'; 
+import { CheckBox } from 'react-native-elements';
 import { Button, Card, CardSection, Input, Spinner } from './common';
 import InvitationCard from './InvitationCard';
 
 class Contact extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: this.props.clicked,
+    };
+  }
   render() {
     return (
-      <View style={{backgroundColor: 'white', width: '100%', height: '100%', borderTopWidth: .4, borderColor: 'grey', minHeight: 80}}>
+      <View style={{backgroundColor: 'white', width: '100%', borderTopWidth: .4, borderColor: 'grey', minHeight: 60}}>
         <View style={{flexDirection: 'row'}}>
           <Image
             style={{width: 50, height: 50, borderRadius: 25, margin: 5}}
@@ -18,6 +26,18 @@ class Contact extends Component {
           <View style={{justifyContent: 'center'}}>
             <Text style={{color:'black', fontWeight: 'bold'}}>George Samson</Text>
             <Text style={{color: 'grey'}}>458-984-0797</Text>
+          </View>
+
+          <View style={{flex: 1, position: "absolute", right: 0, alignSelf: 'flex-end', justifyContent: 'center', alignItems: 'right'}}>
+            <TouchableWithoutFeedback>
+              <CheckBox
+                disabled
+                center
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={this.props.clicked}
+              />
+            </TouchableWithoutFeedback>
           </View>
         </View>
       </View>
--- src/components/Invite.js
+++ src/components/Invite.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View, Image, TouchableHighlight } from 'react-native';
+import { Text, View, Image, TouchableHighlight, TouchableOpacity } from 'react-native';
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import { Button, Card, CardSection, Input, Spinner } from './common';
 import Contact from './Contact';
 class Invite extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 0,
+      selected1: false,
+      selected2: false,
+      selected3: false,
+    };
+  }
+  onPress = () => {
+    console.log('preintggg')
+  }
   render() {
+    var count = 0;
+    console.log(count)
+    console.log(this.state.selected1)
+    if(this.state.selected1) {
+      count = count + 1;
+    }
+    if(this.state.selected2) {
+      count = count + 1;
+    }
+    if(this.state.selected3) {
+      count = count + 1;
+    }
     return (
       <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
         <View style={{justifyContent: 'center', alignItems: 'center'}}>
@@ -19,14 +43,37 @@ class Invite extends Component {
         </View>
         <View style={{width: '100%', flexDirection: 'row', paddingTop: 20}}>
           <Text style={{color: 'grey', textAlign: 'left', width: '50%'}}>Who do you want to invite?</Text>
-          <Text style={{color: 'rgba(106, 207, 103, .9)', textAlign:'right', width: '50%'}}>1 Selected</Text>
+          <Text style={{color: 'rgba(106, 207, 103, .9)', textAlign:'right', width: '50%'}}>{count} Selected</Text>
         </View>
-        <Contact />
+        
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          
+          <TouchableOpacity onPress={() => this.setState({selected1: !this.state.selected1})}> 
+            <Contact 
+              clicked={this.state.selected1}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.setState({selected2: !this.state.selected2})}> 
+            <Contact 
+              clicked={this.state.selected2}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.setState({selected3: !this.state.selected3})}> 
+            <Contact 
+              clicked={this.state.selected3}
+            />
+          </TouchableOpacity>
+          
+        </View>
+        
+
+          
         <TouchableHighlight
           underlayColor={'rgb(100,184,248)'}
           style={{backgroundColor: 'rgb(15,140,255)',position: 'absolute', bottom:0, width: '100%' }}>
           <Text style={{fontSize: 24, textAlign: 'center', color: 'white'}}>Send Invitations</Text>
         </TouchableHighlight>
+
       </View>
     );
   }
